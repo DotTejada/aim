3566ce99e1b197ad581c920831a9fdc609a7a3b9
DotTejada <dotdudeperson@gmail.com>
2025-10-21 13:30:08 -0500

    now just need to work on resizing

--- main.c
+++ main.c
@@ -3,16 +3,23 @@
 #include "stb_image.h"
 
 int main(int argc, char *argv[]) {
+    char brightness_table[10] = {' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'};
     int width, height, channels;
-    unsigned char *data = stbi_load("check.png", &width, &height, &channels, STBI_rgb_alpha);
+    unsigned char *data = stbi_load("check.png", &width, &height, &channels, STBI_grey);
     if (!data) { exit(1); }
-    int size = width * height * channels;
-    printf("width: %d, height: %d, channels: %d\n", width, height, channels);
-    for (int i = 0; i < size; i++) {
-        printf("%d ", data[i]);
-        if (i != 0 && (i + 1) % (width * channels) == 0) {
-            printf("\n");
+
+    int size = width * height * STBI_grey;
+    printf("width: %d, height: %d, channels: %d\n", width, height, STBI_grey);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%c", brightness_table[data[i * width + j]*9/255]);
         }
+        printf("\n");
+    }
+
+    for (int a = 0; a < 150; a++) {
+        printf("#");
     }
     printf("\n");
     stbi_image_free(data);
