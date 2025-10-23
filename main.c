8727d1fb8870f45aa3614d370de75c894843a639
DotTejada <dotdudeperson@gmail.com>
2025-10-22 20:14:47 -0500

    Made it work with transparent images, now might simplify code

--- main.c
+++ main.c
@@ -17,52 +17,60 @@ int main(int argc, char *argv[]) {
     char brightness_table[10] = {' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'};
     int width, height, channels;
 
-    int color;
+    int color_channels;
     if (strcmp(color_flag, "-g") == 0) {
-        color = STBI_grey;
-    } else {
-        color = STBI_rgb;
+        color_channels = STBI_grey_alpha;
+    } else if (strcmp(color_flag, "-c") == 0) {
+        color_channels = STBI_rgb_alpha;
     }
-    unsigned char *data = stbi_load(filename, &width, &height, &channels, color);
+    unsigned char *data = stbi_load(filename, &width, &height, &channels, color_channels);
     if (!data) { 
         printf("Couldn't load file data\n"); 
         exit(1); 
     }
-    //printf("width: %d, height: %d, channels: %d\n", width, height, channels);
 
     int scaled_height = ((float)height / (float)width) * (float)scaled_width / 2;
-    //printf("width: %d, height: %d, channels: %d\n", s_width, s_height, s_channel);
 
     unsigned char *data_scaled = stbir_resize_uint8_srgb(data,  width,  height,  0,
                                                          0, scaled_width, scaled_height, 0,
-                                                         color);
-    if (!data_scaled) { exit(1); }
-    //printf("width: %d, height: %d, channels: %d\n", s_width, s_height, s_channel);
+                                                         color_channels);
+    if (!data_scaled) {
+        printf("Couldn't resize data\n"); 
+        exit(1);
+    }
 
-    if (color == STBI_grey) {
+    if (color_channels == STBI_grey_alpha) {
         for (int i = 0; i < scaled_height; i++) {
-            for (int j = 0; j < scaled_width; j++) {
-                printf("%c", brightness_table[(int)(data_scaled[i * scaled_width + j]*9.0/255.0)]);
+            for (int j = 0; j < scaled_width * color_channels; j += color_channels) {
+
+                int offset = i * scaled_width * color_channels + j;
+                float grey = data_scaled[offset];
+                float a = data_scaled[offset + 1];
+
+                float brightness = grey * a/255.0;
+                printf("%c", brightness_table[(int)(brightness * 9.0/255.0)]);
             }
             printf("\n");
         }
-    } else {
+    } else if (color_channels == STBI_rgb_alpha) {
         // acquire r, g, b from input
         // get brightness from
         // Y = 0.299R + 0.587G + 0.114B
+        // make sure to take alpha into account
         // apply color with
         // 16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
         // print with
         // \x1b[38;5;{ID}m{Char}\x1b[0m
         for (int i = 0; i < scaled_height; i++) {
-            for (int j = 0; j < scaled_width * color; j += 3) {
+            for (int j = 0; j < scaled_width * color_channels; j += color_channels) {
 
-                int value = i * scaled_width * color + j;
-                float r = data_scaled[value];
-                float g = data_scaled[value + 1];
-                float b = data_scaled[value + 2];
+                int offset = i * scaled_width * color_channels + j;
+                float r = data_scaled[offset];
+                float g = data_scaled[offset + 1];
+                float b = data_scaled[offset + 2];
+                float a = data_scaled[offset + 3];
 
-                float brightness = 0.299 * r + 0.587 * g + 0.114 * b;
+                float brightness = (0.299 * r + 0.587 * g + 0.114 * b) * a/255.0;
                 char pixel = brightness_table[(int)(brightness * 9.0/255.0)];
                 r *= 5.0/255.0;
                 g *= 5.0/255.0;
@@ -72,6 +80,7 @@ int main(int argc, char *argv[]) {
             }
             printf("\n");
         }
+    } else {
     }
 
     stbi_image_free(data_scaled);
